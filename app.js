36f5c914c84ca0f2c84bd8e25f4b4278fec61510
mandinga31 <gonzalo.rozas@gmail.com>
2022-02-13 19:26:53 -0300

    arreglando Heroku?

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static('public'));
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3000, function(){
+        console.log("Servidor corriendo en el puerto 3000");
 });
 
+/*app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});*/
+
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.resolve(__dirname + '/views/home.html'));
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(path.resolve(__dirname + '/views/login.html'));
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(path.resolve(__dirname + '/views/register.html'));
 });
